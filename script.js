dc9ff2acaca2f673e47e5e674a528ddf83a999f9
dkalinowski619 <117672327+dkalinowski619@users.noreply.github.com>
2023-01-21 16:15:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function calculate() {
+  var num1 = document.getElementById("num1").value;
+  var num2 = document.getElementById("num2").value;
+  var operation = document.getElementById("operation").value;
+  var result = document.getElementById("result");
+  var finalResult;
+
+  switch(operation) {
+    case "+":
+      finalResult = parseInt(num1) + parseInt(num2);
+      break;
+    case "-":
+      finalResult = parseInt(num1) - parseInt(num2);
+      break;
+    case "*":
+      finalResult = parseInt(num1) * parseInt(num2);
+      break;
+    case "/":
+      finalResult = parseInt(num1) / parseInt(num2);
+      break;
+    default:
+      finalResult = "Invalid Operation";
+  }
+  result.innerHTML = finalResult;
+}
